63cc0abb8be95f30fa20492e45599b2f4c64254d
Carsten Milling <cmil@hashtable.de>
2017-11-30 16:19:28 +0100

    unregister service worker
    (see
    https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#opting-out-of-caching)
    
    This should fix #7 for now.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
+unregister();
