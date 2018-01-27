de9e17544f4a3847cc4e166ec6d1de9903801f4a
Carsten Milling <cmil@hashtable.de>
2018-01-27 19:43:56 +0100

    upgrade to bootstrap 4 and switch to ractstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'whatwg-fetch';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import App from './App';
 import {unregister} from './registerServiceWorker';
