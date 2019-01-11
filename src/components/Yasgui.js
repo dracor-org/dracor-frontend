ff96d95526ccf14629bf1e1941deea17c3252eca
Carsten Milling <cmil@hashtable.de>
2019-01-11 07:22:12 +0100

    add yasgui

--- src/components/Yasgui.js
+++ src/components/Yasgui.js
@@ -1,8 +1,26 @@
 import React, {Component} from 'react';
+import yasgui from 'yasgui/dist/yasgui';
+
+import 'yasgui/dist/yasgui.min.css';
 
 class Yasgui extends Component {
+  componentDidMount () {
+    console.log(yasgui);
+    yasgui(document.getElementById('yasgui'), {
+      yasqe: {
+        sparql: {
+          endpoint: 'https://dracor.org/api/sparql',
+          acceptHeaderSelect: 'application/sparql-results+xml',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        }
+      }
+    });
+  }
+
   render () {
-    return (<div>YASGUI</div>);
+    return <div id="yasgui"/>;
   }
 }
 
