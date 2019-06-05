68eb7e43fbbefc7a2aae0a370c8539685ce684a7
Carsten Milling <cmil@hashtable.de>
2019-06-06 01:07:56 +0200

    refactor Metrics into function component using api

--- src/components/Metrics/index.js
+++ src/components/Metrics/index.js
@@ -1,118 +1,99 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {Row, Col} from 'reactstrap';
-import config from '../../config';
-
-const {apiUrl} = config;
+import api from '../../api';
 
 // Numbers received from the API can be in scientific notation (e.g.
 // 8.248968E6), which is why we need to use parseFloat.
 const fn = val => Number(parseFloat(val)).toLocaleString('en');
 
-class Metrics extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {data: null};
-  }
+const Metrics = () => {
+  const [data, setData] = useState(null);
 
-  componentWillMount () {
-    this.load();
-  }
-
-  load () {
-    const url = `${apiUrl}/corpora?include=metrics`;
-    fetch(url, {})
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        this.setState({data});
-      })
-      .catch(error => {
-        console.log('parsing failed', error);
-      });
-  }
-
-  renderData () {
-    const {data} = this.state;
-    if (!data || data.length === 0) {
-      return null;
+  useEffect(() => {
+    async function fetchData () {
+      try {
+        const response = await api.get('/corpora?include=metrics');
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     }
 
-    return (
-      <Row>
-        {data.map(m => (
-          <Col
-            key={`metrics-${m.name}`}
-            xl={4}
-            lg={6}
-            md={6}
-            sm={12}
-            xs={12}
-          >
-            <h3>
-              <Link to={`/${m.name}`}>{m.title}</Link>
-            </h3>
-            <table className="table">
-              <tbody>
-                <tr>
-                  <th>Number of plays</th>
-                  <td>{fn(m.metrics.plays)}</td>
-                </tr>
-                <tr>
-                  <th>
-                    <code>person</code> + <code>personGrp</code>
-                  </th>
-                  <td>
-                    {fn(m.metrics.characters)}
-                    {
-                      m.metrics.male + m.metrics.female > 0
-                        ? ` (male: ${m.metrics.male}, female: ${m.metrics.female})`
-                        : ''
-                    }
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    <code>text</code>
-                  </th>
-                  <td>{fn(m.metrics.wordcount.text)} tokens</td>
-                </tr>
-                <tr>
-                  <th>
-                    <code>sp</code>
-                  </th>
-                  <td>
-                    {fn(m.metrics.sp)} ({fn(m.metrics.wordcount.sp)} tokens)
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    <code>stage</code>
-                  </th>
-                  <td>
-                    {fn(m.metrics.stage)} ({fn(m.metrics.wordcount.stage)} tokens)
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    Last update
-                  </th>
-                  <td>{(new Date(m.metrics.updated)).toLocaleString()}</td>
-                </tr>
-              </tbody>
-            </table>
-          </Col>
-        ))}
-      </Row>
-    );
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <p>Loading...</p>;
   }
 
-  render () {
-    const {data} = this.state;
-    return data ? this.renderData() : <em>loading...</em>;
-  }
-}
+  return (
+    <Row>
+      {data.map(m => (
+        <Col
+          key={`metrics-${m.name}`}
+          xl={4}
+          lg={6}
+          md={6}
+          sm={12}
+          xs={12}
+        >
+          <h3>
+            <Link to={`/${m.name}`}>{m.title}</Link>
+          </h3>
+          <table className="table">
+            <tbody>
+              <tr>
+                <th>Number of plays</th>
+                <td>{fn(m.metrics.plays)}</td>
+              </tr>
+              <tr>
+                <th>
+                  <code>person</code> + <code>personGrp</code>
+                </th>
+                <td>
+                  {fn(m.metrics.characters)}
+                  {
+                    m.metrics.male + m.metrics.female > 0
+                      ? ` (male: ${m.metrics.male}, female: ${m.metrics.female})`
+                      : ''
+                  }
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <code>text</code>
+                </th>
+                <td>{fn(m.metrics.wordcount.text)} tokens</td>
+              </tr>
+              <tr>
+                <th>
+                  <code>sp</code>
+                </th>
+                <td>
+                  {fn(m.metrics.sp)} ({fn(m.metrics.wordcount.sp)} tokens)
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  <code>stage</code>
+                </th>
+                <td>
+                  {fn(m.metrics.stage)} ({fn(m.metrics.wordcount.stage)} tokens)
+                </td>
+              </tr>
+              <tr>
+                <th>
+                  Last update
+                </th>
+                <td>{(new Date(m.metrics.updated)).toLocaleString()}</td>
+              </tr>
+            </tbody>
+          </table>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
 export default Metrics;
