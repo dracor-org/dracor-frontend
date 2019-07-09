9b6f751e131f3899dce8c603c266faad33c5ef1d
Carsten Milling <cmil@hashtable.de>
2019-07-09 14:09:57 +0200

    update credits on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,9 +12,9 @@ const Home = () => (
       <small>
         <em>
           {`Our two in-house TEI corpora (RusDraCor and GerDraCor) hosted on
-            dracor.org are in public-alpha state. Feel free to use them, but
+            dracor.org are in public-beta state. Feel free to use them, but
             there are some issues that still have to be resolved before the
-            official release, which is planned for the second half of 2019. –
+            official release, which is planned for the end of 2019. –
             SpanDraCor is based on our fork of the `
           }
           <a href="https://github.com/GHEDI/BETTE">
@@ -24,15 +24,25 @@ const Home = () => (
           <a href="https://www.folgerdigitaltexts.org/">
             Shakespeare Folger Library
           </a>
+          {', GreekDraCor and RomDraCor from the '}
+          <a href="http://www.perseus.tufts.edu/hopper/opensource/download">
+            Perseus Digital Library
+          </a>
           {'. – dracor.org is edited by '}
           <a href="https://www.hse.ru/en/org/persons/182492735">
             Frank&nbsp;Fischer
           </a>
-          {' (Higher School of Economics, Moscow) and '}
+          {' (Higher School of Economics, Moscow), '}
           <a href="https://www.uni-potsdam.de/de/lit-19-jhd/peertrilcke.html">
             Peer&nbsp;Trilcke
           </a>
-          {' (University of Potsdam).'}
+          {' (University of Potsdam) and '}
+          <a href="http://nevmenandr.net/bo.php">
+            Boris&nbsp;Orekhov
+          </a>
+          {' (Higher School of Economics, Moscow).'}
+          <br/>
+          {'Technical lead: Carsten Milling (Berlin).'}
         </em>
       </small>
     </div>
