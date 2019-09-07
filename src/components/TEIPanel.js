2697571f1ad7fed2f181018fe8ce24ed01f17a0b
Carsten Milling <cmil@hashtable.de>
2019-09-07 16:43:55 +0300

    add CETEIcean style sheet
    This is CETEIcean/css/CETEIcean.css with styling for the html element
    removed.

--- src/components/TEIPanel.js
+++ src/components/TEIPanel.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import CETEI from 'CETEIcean';
 
+import '../CETEIcean.css';
+
 const TEIPanel = ({url}) => {
   const CETEIcean = new CETEI();
   CETEIcean.getHTML5(url, data => {
