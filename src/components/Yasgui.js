a6e2068793ebd24da522794dd4780c0bb08c6d13
Carsten Milling <cmil@hashtable.de>
2019-12-23 01:23:22 +0100

    change default query

--- src/components/Yasgui.js
+++ src/components/Yasgui.js
@@ -12,17 +12,11 @@ yasgui.defaults.catalogueEndpoints = [
   {endpoint: 'https://query.wikidata.org/sparql', title: 'Wikidata'}
 ];
 
-yasgui.YASQE.defaults.value = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-PREFIX wd: <http://www.wikidata.org/entity/>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX dc: <http://purl.org/dc/elements/1.1/>
-PREFIX dracon: <http://dracor.org/ontology#>
-SELECT * WHERE {
+yasgui.YASQE.defaults.value = `PREFIX urn: <http://fliqz.com/>
+SELECT *  FROM <urn:x-arq:UnionGraph> WHERE {
   ?sub ?pred ?obj .
 }
-LIMIT 10`;
+LIMIT 50`;
 
 class Yasgui extends Component {
   componentDidMount () {
