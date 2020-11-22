da64ff5b43af4691d7fef2e929243c2ea6090d89
Carsten Milling <cmil@hashtable.de>
2020-11-22 18:11:27 +0100

    Move footer into its own component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,79 +1,12 @@
-import React, {useContext} from 'react';
-import {DracorContext} from '../context';
+import React from 'react';
 import Corpora from './Corpora';
+import Footer from './Footer';
 
 const Home = () => {
-  const {apiInfo} = useContext(DracorContext);
-
   return (
     <div>
       <Corpora/>
-      <div className="footer">
-        <div className="citation">
-          <h6>To cite DraCor, please use:</h6>
-          <p>
-            Frank Fischer et al.:
-            {' '}
-            <a target="_blank" rel="noopener noreferrer" href="https://dev.clariah.nl/files/dh2019/boa/0268.html">
-              Programmable Corpora: Introducing DraCor, an Infrastructure for the
-              Research on European Drama.
-            </a>
-            <br/>In <i>Proceedings of DH2019</i>, Utrecht University.
-          </p>
-        </div>
-        <div className="current-year">
-          <p>Drama Corpora Project <br/>2020</p>
-        </div>
-        <div className="license">
-          <a
-            href="https://creativecommons.org/publicdomain/zero/1.0"
-            rel="noopener noreferrer license"
-            target="_blank"
-          >
-            <img src="img/cc0.svg" alt="CC0"/>
-          </a>
-          <br/>Unless otherwise stated, all corpora and the web design are released under
-          {' '}
-          <span className="version-pill">
-            <span>Creative Commons</span>
-            <a
-              href="https://creativecommons.org/publicdomain/zero/1.0"
-              rel="noopener noreferrer license"
-              target="_blank"
-            >
-              0 1.0
-            </a>
-          </span>
-          {apiInfo.version && (
-            <p className="api-info">
-              This site runs on
-              {' '}
-              <span className="version-pill">
-                <span>DraCor API</span>
-                <a
-                  href={`https://github.com/dracor-org/dracor-api/releases/tag/v${apiInfo.version}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {apiInfo.version}
-                </a>
-              </span>
-              {' using '}
-              <span className="version-pill">
-                <span>eXist-db</span>
-                <a
-                  href={`https://github.com/eXist-db/exist/releases/tag/eXist-${apiInfo.existdb}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {apiInfo.existdb}
-                </a>
-              </span>
-            </p>
-          )}
-        </div>
-
-      </div>
+      <Footer/>
     </div>
   );
 };
