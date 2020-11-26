c047599a45e7a88d99c6efb78e65b832346ab9df
markschwindt <mark.schwindt@rub.de>
2020-11-26 20:21:23 +0100

    redesign footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,22 +7,21 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="citation">
-        <h6>
-          If you want to cite DraCor, please use the following reference ({/**/}
-          <a
-            href={`${process.env.PUBLIC_URL}/cite_dracor.bib`}
-            type="application/x-bibtex"
-          >
-            BibTeX
-          </a>,
-          {' '}
-          <a
-            href={`${process.env.PUBLIC_URL}/cite_dracor.ris`}
-            type="application/x-research-info-systems"
-          >
-            RIS
-          </a>):
-        </h6>
+        <h5>
+          If you want to cite DraCor, <wbr/>please use the following reference:
+        </h5>
+        <a
+          href={`${process.env.PUBLIC_URL}/cite_dracor.bib`}
+          type="application/x-bibtex"
+        >
+          <img src="../img/bib-icon.svg"/>
+        </a>
+        <a
+          href={`${process.env.PUBLIC_URL}/cite_dracor.ris`}
+          type="application/x-research-info-systems"
+        >
+          <img src="../img/ris-icon.svg"/>
+        </a>
         <p>
           Fischer, Frank, et al. (2019). Programmable Corpora: Introducing
           DraCor, an Infrastructure for the Research on European Drama. In{' '}
@@ -37,21 +36,10 @@ const Footer = () => {
           </a>.
         </p>
       </div>
-      <div className="current-year">
-        <p>Drama Corpora Project <br/>2020</p>
-      </div>
       <div className="license">
-        <a
-          href="https://creativecommons.org/publicdomain/zero/1.0"
-          rel="noopener noreferrer license"
-          target="_blank"
-        >
-          <img src="img/cc0.svg" alt="CC0"/>
-        </a>
-        <br/>Unless otherwise stated, all corpora and the web design are released under
-        {' '}
-        <span className="version-pill">
-          <span>Creative Commons</span>
+        <h5>Drama Corpora Project 2020</h5>
+        <p>Unless otherwise stated, all corpora and the web design<br/> are released under Creative Commons
+          {' '}
           <a
             href="https://creativecommons.org/publicdomain/zero/1.0"
             rel="noopener noreferrer license"
@@ -59,7 +47,14 @@ const Footer = () => {
           >
             0 1.0
           </a>
-        </span>
+          <a
+            href="https://creativecommons.org/publicdomain/zero/1.0"
+            rel="noopener noreferrer license"
+            target="_blank"
+          >
+            <img src="img/cc0.svg" alt="CC0"/>
+          </a>
+        </p>
         {apiInfo.version && (
           <p className="api-info">
             This site runs on
@@ -74,6 +69,7 @@ const Footer = () => {
                 {apiInfo.version}
               </a>
             </span>
+            <wbr/>
             {' using '}
             <span className="version-pill">
               <span>eXist-db</span>
