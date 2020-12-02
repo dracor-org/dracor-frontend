27ab2556c0ba36078b088a221287fda8814f54c8
Carsten Milling <cmil@hashtable.de>
2020-12-03 00:04:58 +0100

    Implement page title for markdown pages

--- src/components/DocPage.js
+++ src/components/DocPage.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import ReactMarkdown from 'react-markdown';
+import {Helmet} from 'react-helmet';
 import axios from 'axios';
 import Footer from './Footer';
 
 const DocPage = ({match}) => {
   const [markdown, setMarkdown] = useState('');
+  const [title, setTitle] = useState('');
   const {slug} = match.params;
 
   useEffect(() => {
@@ -17,12 +19,19 @@ const DocPage = ({match}) => {
           response.headers['content-type'].match(/^text\/markdown/)
         ) {
           setMarkdown(response.data);
+          const firstLine = response.data
+            .replace(/^\s+/, '')
+            .split(/(\n|\r|\r\n){2,}/)[0];
+          const m = firstLine.match(/^#\s*(.+)/);
+          setTitle(m ? m[1] : '...');
         } else {
           setMarkdown('Not Found');
+          setTitle('Not Found');
         }
       } catch (error) {
         if (error.message === 'Request failed with status code 404') {
           setMarkdown('Not Found');
+          setTitle('Not Found');
         } else {
           console.error(error);
         }
@@ -36,6 +45,9 @@ const DocPage = ({match}) => {
 
   return (
     <div className="dracor-page">
+      <Helmet titleTemplate="%s - DraCor">
+        <title>{title}</title>
+      </Helmet>
       <ReactMarkdown source={markdown}/>
       <Footer/>
     </div>
