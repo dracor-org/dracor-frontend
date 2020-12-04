b4359ff8f25e48bcfcb58b0a09d13ee4d979d246
Carsten Milling <cmil@hashtable.de>
2020-12-04 14:33:24 +0100

    Adjust right and left column of the play details header when sticky

--- src/components/PlayDetailsHeader.js
+++ src/components/PlayDetailsHeader.js
@@ -24,7 +24,7 @@ const PlayDetailsHeader = ({play}) => {
 
   return (
     <Header className={cx('main')}>
-      <Col md="8">
+      <Col className={cx('left')}>
         <h1>{title}</h1>
 
         {subtitle && <h2 className={cx('subtitle')}>{subtitle}</h2>}
