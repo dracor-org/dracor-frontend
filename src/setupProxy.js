2404737677de1e6b9d3655cc526197c1f3b114fa
Carsten Milling <cmil@hashtable.de>
2023-05-22 00:20:55 -0700

    Make dev server proxy path configurable

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,13 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
+const proxyPath = process.env.PROXY_PATH || '/exist/restxq/v1/';
+
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
       target: 'http://localhost:8080/',
       pathRewrite: {
-        '^/api/': '/exist/restxq/',
+        '^/api/': proxyPath,
       },
     })
   );
