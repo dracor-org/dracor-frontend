b918d188ce2f0894de93c21b6a023adeec91dcd0
Carsten Milling <cmil@hashtable.de>
2024-06-01 02:28:02 +0200

    Implement Commit component

---
+++ src/components/Commit.tsx
@@ -0,0 +1,26 @@
+import classnames from 'classnames/bind';
+import style from './Commit.module.scss';
+
+const cx = classnames.bind(style);
+
+export interface Props {
+  repo?: string;
+  children: string;
+}
+
+export default function Commit({repo, children}: Props) {
+  const sha = children.trim();
+  const short = sha.substring(0, 8);
+  const url = `${repo}/commit/${sha}`;
+
+  return (
+    <>
+      {repo && (
+        <a href={url} target="_blank">
+          {short}
+        </a>
+      )}
+      {!repo && <span>{short}</span>}
+    </>
+  );
+}
