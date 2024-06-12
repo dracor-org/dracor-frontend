92a24f597c09649fd0d6ece80c14c019199306d5
Carsten Milling <cmil@hashtable.de>
2024-06-12 16:12:28 +0200

    Add Helmet to ODDPage

--- src/components/OddPage.js
+++ src/components/OddPage.js
@@ -1,4 +1,5 @@
 import {Container} from 'reactstrap';
+import {Helmet} from 'react-helmet';
 import Header from './Header';
 import Footer from './Footer';
 import Odd from './Odd';
@@ -6,6 +7,9 @@ import Odd from './Odd';
 const OddPage = () => {
   return (
     <Container fluid>
+      <Helmet titleTemplate="%s - DraCor">
+        <title>ODD</title>
+      </Helmet>
       <div className="dracor-page">
         <Header>DraCor ODD</Header>
         <Odd />
