3bc9519ddd6f0b7e058bb8df2f908c27824ed35e
Carsten Milling <cmil@hashtable.de>
2024-08-11 19:30:15 +0200

    Change order of imports to fix precedence of style definitions

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import {createRoot} from 'react-dom/client';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
