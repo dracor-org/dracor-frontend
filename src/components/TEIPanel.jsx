c056b25967f234ed91691584db7887b1cdbd3d62
Carsten Milling <cmil@hashtable.de>
2025-01-31 13:08:15 +0100

    Load TEI in useEffect and replaceChildren instead append
    resolve #324

--- src/components/TEIPanel.jsx
+++ src/components/TEIPanel.jsx
@@ -1,18 +1,24 @@
+import {useRef, useEffect} from 'react';
 import CETEI from 'CETEIcean';
 
 import '../CETEIcean.css';
 import '../CETEIcean.scss';
 
+const CETEIcean = new CETEI();
+
 const TEIPanel = ({url}) => {
-  const CETEIcean = new CETEI();
-  CETEIcean.getHTML5(url, (data) => {
-    document.querySelector('#TEI').append(data);
-  });
+  const ref = useRef(null);
+
+  useEffect(() => {
+    CETEIcean.getHTML5(url, (data) => {
+      ref.current.replaceChildren(data);
+    });
+  }, [url]);
 
   return (
     <div className="tei-frame">
       <div className="ceteicean dracor-scrollbar">
-        <div id="TEI" />
+        <div id="TEI" ref={ref} />
       </div>
     </div>
   );
